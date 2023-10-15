a9e061eae827e9fea77e698d2aafff357a4194d1
HossainEhs <hossainehsani@outlook.com>
2023-10-15 02:09:50 -0400

    chnages in yml

--- app/src/main/java/com/hossainehs/ktlintexample/MainActivity.kt
+++ app/src/main/java/com/hossainehs/ktlintexample/MainActivity.kt
@@ -7,7 +7,7 @@ import com.hossainehs.ktlint_example.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // comment
+        setContentView(R.layout.activity_main) //comment
 
 
     }
